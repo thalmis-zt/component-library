b61c50049e6b7e0b35540e2f0fc7997b28dcc41a
ThalmisFaraj <thalmis.faraj@zaeemsolutions.com>
2024-07-19 08:03:27 +0530

    button updated

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -3,3 +3,13 @@
 </script>
 
 <button>{btnText}</button>
+
+<style>
+  button {
+    background-color: blueviolet;
+    color: white;
+    font-weight: bold;
+    padding: 12px 32px;
+    font-size: 14px;
+  }
+</style>
